49cfc6d2d3789c70d4d5156e923c9eb1a4a33d88
chosen <67898034+param-ninthu@users.noreply.github.com>
2024-07-12 01:43:43 +0530

    rest- google calendar api task

---
+++ rest/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use("/api/events", require("./routes/eventRoutes"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
